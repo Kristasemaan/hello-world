15d13f531684669268a7375795b92861affc0989
Kristasemaan <k.semaan@wustl.edu>
2024-11-07 15:33:09 -0600

    render square component instead of x button in board

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-function Sqaure(){
+function Sqaure() {
   return <button className="square">1</button>;
 }
 
@@ -6,19 +6,19 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
+        <Sqaure />
+        <Sqaure />
+        <Sqaure />
       </div>
       <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
+        <Sqaure />
+        <Sqaure />
+        <Sqaure />
       </div>
       <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Sqaure />
+        <Sqaure />
+        <Sqaure />
       </div>
     </>
   );
